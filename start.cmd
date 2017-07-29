313343d4f070dd32561572fa98e6b5ebd194b2ae
Asuna-Yuuki <Wesleyvanneck1234@gmail.com>
2017-07-29 12:39:41 +0200

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,39 +1,28 @@
-param (
-	[switch]$Loop = $false
+@echo off
+TITLE rocky server software for Minecraft: Pocket Edition
+cd /d %~dp0
+
+if exist bin\php\php.exe (
+	set PHPRC=""
+	set PHP_BINARY=bin\php\php.exe
+) else (
+	set PHP_BINARY=php
 )
 
-if(Test-Path "bin\php\php.exe"){
-	$env:PHPRC = ""
-	$binary = "bin\php\php.exe"
-}else{
-	$binary = "php"
-}
-if(Test-Path "Rocky.phar"){
-	$file = "Rocky.phar"
-}elseif(Test-Path "src\pocketmine\PocketMine.php"){
-	$file = "src\pocketmine\PocketMine.php"
-}else{
-	echo "I couldn't find Rocky's src or phar."
-	pause
-	exit 1
-}
+if exist rocky.phar (
+	set POCKETMINE_FILE=rocky.phar
+) else (
+	if exist src\pocketmine\PocketMine.php (
+		set POCKETMINE_FILE=src\pocketmine\PocketMine.php
+	) else (
+		echo "Couldn't find a valid PocketMine-MP installation"
+		pause
+		exit 1
+	)
+)
 
-function StartServer{
-	$command = $binary + " " + $file  + " --enable-ansi"
-    chcp 65001
-	iex $command
-}
-
-$loops = 0
-
-StartServer
-
-while($Loop){
-	if($loops -ne 0){
-		echo ("Restarted " + $loops + " times")
-	}
-	$loops++
-	echo "To escape the loop, press CTRL+C now. Otherwise, wait 5 seconds for the server to restart."
-	echo ""
-	StartServer
-}
+if exist bin\mintty.exe (
+	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "rocky" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
+) else (
+	%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
+)
